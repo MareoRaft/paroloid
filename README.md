6b7b824fd037df6734634c65cedba661b08d397b
Matthew Lancellotti <mvlancellotti@gmail.com>
2022-02-26 22:56:54 -0500

    feat: basic built website working

---
+++ README.md
@@ -0,0 +1,2 @@
+The goal is to program an Italian version of Wordoid using Go.
+
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+//
+import (
+	"syscall/js"
+)
+//
+func main() {
+  js.Global().Call("alert", "Hello World")
+// 	println("Hello, JS console")
+}
